370aa918ce9e8a91ebc90e94bb6247a84d7b5ef0
sandra perez <sandra.perez@enti.cat>
2024-04-01 23:16:42 +0200

    Revert "Merge remote-tracking branch 'origin/feature/victor' into feature/sandra"
    This reverts commit ceb69fe665a9fe9b4a2d38dc8f41cc434f833356, reversing
    changes made to f55b92c98676c00cac733847f83f78fcfe625556.

--- Assets/Scripts/UI/Settings/VolumeLogic.cs
+++ Assets/Scripts/UI/Settings/VolumeLogic.cs
@@ -1,35 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class VolumeLogic : MonoBehaviour
 {
-    public Slider slider;
-    public float sliderValue;
-    public Image music;
-
+    // Start is called before the first frame update
     void Start()
     {
-        slider.value = PlayerPrefs.GetFloat("Volume", 0.5f);
-        AudioListener.volume = slider.value;
-        CheckMute();
+        
     }
 
-    public void ChangeSlider(float value)
+    // Update is called once per frame
+    void Update()
     {
-        sliderValue = value;
-        PlayerPrefs.SetFloat("Volume", sliderValue);
-        AudioListener.volume = slider.value;
-        CheckMute();
-    }
-
-    public void CheckMute()
-    {
-        if (sliderValue == 0)
-            music.enabled = true;
-
-        else
-            music.enabled = false;
+        
     }
 }
