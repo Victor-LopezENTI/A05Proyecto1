1c5707b869e122956f80a3ec28ddd1b963382d4f
Víctor López Guerra <victor.lopez.guerra@enti.cat>
2024-05-07 02:26:30 +0200

    All Good Scenes Modified & Almost Done
    Still requiers changes and asjustments

--- Assets/Resources/Scripts/AudioManager.cs
+++ Assets/Resources/Scripts/AudioManager.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource MainMenuMusic;
     [SerializeField] AudioSource SFXMenuMusic;
 
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
+    private bool menu;
 
     void Start()
     {
         MainMenuMusic.Play();
+    }
+
+    private void Update()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+
+        if (scene.name == "MainMenuScene")
+            Destroy(gameObject);
+
+        else
+            DontDestroyOnLoad(gameObject);
+
     }
 
 }
