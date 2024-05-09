9f4837223b9a8876c6f5cdefba0ed9db1090806c
sandra perez <sandra.perez@enti.cat>
2024-05-10 00:36:02 +0200

    soul counter ui

--- Assets/Resources/Scripts/Player/SoulSpheresCollector.cs
+++ Assets/Resources/Scripts/Player/SoulSpheresCollector.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SoulSpheresCollector : MonoBehaviour
 {
-    private int soulSphereCounter = 0;
+    public int soulSphereCounter = 0;
+    public TMP_Text counterText;
    
+    void Start()
+    {
+        soulSphereCounter = PlayerPrefs.GetInt("SoulSphereCounter", 0);
+        counterText.text =  soulSphereCounter.ToString();
+    }
 
     void OnTriggerEnter2D(Collider2D soulSphere)
     {
         if (soulSphere.gameObject.CompareTag("Soul")) 
         {
+            PlayerPrefs.SetInt("SoulSphereCounter", soulSphereCounter);
             Debug.Log("Soul Sphere Collected. Counter: " + ++soulSphereCounter);
-            Destroy(soulSphere.gameObject); 
+            Destroy(soulSphere.gameObject);
+            counterText.text = soulSphereCounter.ToString();
         }
     }
 
