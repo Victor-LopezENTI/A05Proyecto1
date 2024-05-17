c09ce0a63cc3dd065b82966611434c65ca5a543b
sandra perez <sandra.perez@enti.cat>
2024-05-17 17:03:39 +0200

    Revert "Merge remote-tracking branch 'origin/develop' into feature/sandra2"
    This reverts commit 8c3a12c7d0434ed79733ecf4f022d1eccea09905, reversing
    changes made to 98d4acd9a9a920bcf427f49b6f34aa5c3ffca8a0.

--- Assets/Resources/Scripts/UI/PauseMenu.cs
+++ Assets/Resources/Scripts/UI/PauseMenu.cs
@@ -1,6 +1,4 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
@@ -23,12 +21,6 @@ public class PauseMenu : MonoBehaviour
             DontDestroyOnLoad(gameObject.transform.root.gameObject);
         }
     }
-
-    private void OnEnable()
-    {
-        InputManager.PlayerInputActions.Player.Reset.performed += RestartGame;
-    }
-
     private void Start()
     {
         pauseMenuUI = GameUIManager.instance.pauseUI;
@@ -37,7 +29,8 @@ public class PauseMenu : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && settingsON == false)
+        /*
+         * if (Input.GetKeyDown(KeyCode.Escape) && settingsON == false)
         {
             if (paused)
                 ResumeGame();
@@ -45,8 +38,15 @@ public class PauseMenu : MonoBehaviour
             else
                 PausedMenu();
         }
+        if (InputManager.instance.resetInput && !paused)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SoulSpheresCollector.instance.soulSphereCounter -= SoulSpheresCollector.instance.sceneSphereCounter;
+            SoulSpheresCollector.instance.sceneSphereCounter = 0;
+        }
+         */
     }
-
+    
 
     private void PausedMenu()
     {
@@ -55,7 +55,7 @@ public class PauseMenu : MonoBehaviour
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
         AudioManager.Instance.PlaySFX("Pause");
-        //  PlayerStateMachine.instance.isPaused = true;
+      //  PlayerStateMachine.instance.isPaused = true;
     }
 
     public void ResumeGame()
@@ -63,20 +63,11 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(false);
         paused = false;
         Cursor.visible = false;
-        // PlayerStateMachine.instance.isPaused = false;
+       // PlayerStateMachine.instance.isPaused = false;
     }
-
-    private void RestartGame(InputAction.CallbackContext context)
-    {
-        if (paused) return;
-
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        SoulSpheresCollector.instance.soulSphereCounter -= SoulSpheresCollector.instance.sceneSphereCounter;
-        SoulSpheresCollector.instance.sceneSphereCounter = 0;
-    }
-
     public void EnterSettings()
     {
+        
     }
 
     public void ExitSettings()
@@ -91,10 +82,5 @@ public class PauseMenu : MonoBehaviour
         SceneController.instance.LoadScene("MainMenuScene");
         AudioManager.Instance.StopMusic();
         AudioManager.Instance.PlayMusic("Initial");
-    }
-
-    private void OnDisable()
-    {
-        InputManager.PlayerInputActions.Player.Reset.performed -= RestartGame;
     }
 }
