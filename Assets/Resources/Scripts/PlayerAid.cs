c18a7e5a5ff8d0786d9f24611a3ce94e6fe4d540
Pol Lleida <pol.lleida@enti.cat>
2024-05-27 17:49:47 +0200

    Player Aid particles

--- Assets/Resources/Scripts/PlayerAid.cs
+++ Assets/Resources/Scripts/PlayerAid.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 public class PlayerAid : MonoBehaviour
 {
     private GameObject _destination;
+    private ParticleSystem _particleSystem;
     private int _fallCount;
+    [SerializeField] private int fallCountMax;
 
     private void Awake()
     {
         _destination = transform.Find("Destination").gameObject;
+        _destination.SetActive(false);
+        _particleSystem = _destination.GetComponent<ParticleSystem>();
         _fallCount = 0;
     }
-    
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!_destination) return;
@@ -19,20 +23,27 @@ public class PlayerAid : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             _fallCount++;
-            if (_fallCount >= 5)
+            if (_fallCount >= fallCountMax)
             {
-                PlayerStateMachine.instance.isPaused = true;
-                PlayerStateMachine.instance.playerRb.excludeLayers = LayerMask.GetMask("Platforms");
-                PlayerStateMachine.instance.playerRb.DOMove(_destination.transform.position, 1.5f).onComplete =
-                    UnlockPlayer;
+                MoveToDestination();
             }
-
-            Debug.Log(_fallCount);
         }
+    }
+
+    private void MoveToDestination()
+    {
+        _destination.SetActive(true);
+        _particleSystem.Play();
+        PlayerStateMachine.instance.isPaused = true;
+        PlayerStateMachine.instance.playerRb.excludeLayers = LayerMask.GetMask("Platforms");
+        PlayerStateMachine.instance.playerRb.DOMove(_destination.transform.position, 1.5f).onComplete =
+            UnlockPlayer;
     }
 
     private void UnlockPlayer()
     {
+        _destination.SetActive(false);
+        _particleSystem.Stop();
         PlayerStateMachine.instance.playerRb.excludeLayers = 0;
         PlayerStateMachine.instance.isPaused = false;
     }
