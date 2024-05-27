abea1acc4be9f8f2bfe40e32d917134eb1399c71
Pol Lleida <pol.lleida@enti.cat>
2024-05-27 16:56:19 +0200

    Dark props

--- Assets/Resources/Scripts/PlayerAid.cs
+++ Assets/Resources/Scripts/PlayerAid.cs
@@ -1,39 +1,39 @@
-using System;
 using DG.Tweening;
 using UnityEngine;
 
 public class PlayerAid : MonoBehaviour
 {
-	private GameObject _destination;
-	private Collider2D _collider2D;
-	private int _fallCount;
+    private GameObject _destination;
+    private int _fallCount;
 
-	private void Awake()
-	{
-		_destination = transform.Find("Destination").gameObject;
-		_collider2D = GetComponent<Collider2D>();
-		_fallCount = 0;
-	}
+    private void Awake()
+    {
+        _destination = transform.Find("Destination").gameObject;
+        _fallCount = 0;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!_destination) return;
 
+        if (other.CompareTag("Player"))
+        {
+            _fallCount++;
+            if (_fallCount >= 5)
+            {
+                PlayerStateMachine.instance.isPaused = true;
+                PlayerStateMachine.instance.playerRb.excludeLayers = LayerMask.GetMask("Platforms");
+                PlayerStateMachine.instance.playerRb.DOMove(_destination.transform.position, 1.5f).onComplete =
+                    UnlockPlayer;
+            }
 
-	private void OnTriggerEnter2D(Collider2D other)
-	{
-		if (other.CompareTag("Player"))
-		{
-			_fallCount++;
-			if (_fallCount >= 5)
-			{
-				PlayerStateMachine.instance.playerRb.excludeLayers = LayerMask.GetMask("Platforms");
-				PlayerStateMachine.instance.playerRb.DOMove(_destination.transform.position, 1.5f).onComplete =
-					ExcludePlayerFromPlatforms;
-			}
+            Debug.Log(_fallCount);
+        }
+    }
 
-			Debug.Log(_fallCount);
-		}
-	}
-
-	private void ExcludePlayerFromPlatforms()
-	{
-		PlayerStateMachine.instance.playerRb.excludeLayers = 0;
-	}
+    private void UnlockPlayer()
+    {
+        PlayerStateMachine.instance.playerRb.excludeLayers = 0;
+        PlayerStateMachine.instance.isPaused = false;
+    }
 }
